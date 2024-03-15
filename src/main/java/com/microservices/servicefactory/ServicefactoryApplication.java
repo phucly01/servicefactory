d4cbf280631c10a0fa577b9a436ca690f8eb1a62
phucly01 <phongthuypts@yahoo.com>
2024-03-14 21:22:18 -0400

    initial commit

---
+++ src/main/java/com/microservices/servicefactory/ServicefactoryApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.servicefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicefactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicefactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/servicefactory/ServicefactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.servicefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicefactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
